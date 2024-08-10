323486d958c592b346c4fd28447393283932a842
Amrishkanna R <122910294+ItsAmrish@users.noreply.github.com>
2024-08-10 23:57:15 +0530

    Add files via upload

---
+++ Task 5/Todo-backend/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB:', err.message, err.stack);
+  });
+
+const todoSchema = new mongoose.Schema({ 
+  title: {
+    required: true,
+    type: String
+  }, 
+  description: String 
+});
+
+const todoModel = mongoose.model('Todo', todoSchema);
+
+
+
+app.post('/todos', async (req, res) => {
+  const { title, description } = req.body;
+  try {
+    const newTodo = new todoModel({ title, description });
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await todoModel.find();
+    res.json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/todos/:id", async(req, res) => {
+  try{
+    const { title, description } = req.body;
+    const id = req.params.id;
+    const updatedTodo =  await todoModel.findByIdAndUpdate(
+      id,
+      { title, description },
+      {new: true}
+    )
+    if (!updatedTodo){
+      return res.status(404).json({ message: "Todo not found" }); 
+    }
+    res.json(updatedTodo)
+  } catch (error){
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+  
+})
+
+app.delete('/todos/:id', async(req, res) => {
+  try{
+    const id = req.params.id;
+    await todoModel.findByIdAndDelete(id)
+    res.status(204).end();
+  } catch (error){
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+  
+})
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
